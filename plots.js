3228d192cb87fb1ab091604efedf21028d95d05a
ines-tb <ines12tb@gmail.com>
2020-08-23 10:11:43 -0500

    Bar and Bubble charts

--- plots.js
+++ plots.js
@@ -1,42 +1,99 @@
 function init() {
-    var selector = d3.select("#selDataset");
-  
-    d3.json("samples.json").then((data) => {
-      var sampleNames = data.names;
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  })}
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+    
+    optionChanged(selector.property("value"));
+
+})}
 
 function optionChanged(newSample) {
-    buildMetadata(newSample);
-    buildCharts(newSample);
+  
+  d3.json("samples.json").then((data) => {
+    buildMetadata(newSample,data);
+    buildCharts(newSample,data);
+  })
 };
 
-function buildMetadata(sample){
+function buildMetadata(sample,data){
+    
     console.log("Sample displayed: " + sample);
 
-    d3.json("samples.json").then((data) => {
-        var metadata = data.metadata;
-        var resultArray  = metadata.filter(sampleElement => sampleElement.id == sample);
-        var result = resultArray [0];
-        var PANEL = d3.select("#sample-metadata");
+    var metadata = data.metadata;
+    var resultArray  = metadata.filter(sampleElement => sampleElement.id == sample);
+    var result = resultArray [0];
+    var PANEL = d3.select("#sample-metadata");
 
-        // Clear panel before assigning
-        PANEL.html("");
+    // Clear panel before assigning
+    PANEL.html("");
 
-        Object.entries(result).forEach(([key,value]) => {
-            PANEL.append("h6").text(key.toUpperCase() + " : " + value);       
-        });
+    Object.entries(result).forEach(([key,value]) => {
+        PANEL.append("h6").text(key.toUpperCase() + " : " + value);
+    }); 
 
-    })
+
 };
 
-function buildCharts(sample){
+function buildCharts(sample,data){
 
+    var samples = data.samples;
+    var resultArray  = samples.filter(sampleElement => sampleElement.id == sample);
+    var result = resultArray [0];
+
+    // BAR CHART:
+    // Top ten bacterial species
+    
+    var topNames = result.otu_ids.slice(0,10).reverse().map(id => "OTU " + id);
+    var topValues = result.sample_values.slice(0,10).reverse();
+    var topLabels = result.otu_labels.slice(0,10);
+
+    var trace1 = {
+        y: topNames,
+        x: topValues,
+        text: topLabels,
+        type: "bar",
+        orientation: "h"
+    };
+
+    var plotData1 = [trace1];
+
+    var layout1 = {
+        title: "Top 10 Bacterial Species Found"
+    }
+    
+    Plotly.newPlot("bar", plotData1, layout1);
+    
+
+    // BUBBLE CHART 
+    // All bacterial found
+
+    var trace2 = {
+        x: result.otu_ids,
+        y: result.sample_values,
+        text: result.otu_labels,
+        mode: 'markers',          
+        marker: {
+          size: result.sample_values,
+          color: result.otu_ids              
+        }
+    }
+      
+    var plotData2 = [trace2];
+    
+    var layout2 = {
+        title: 'Bacteria Cultures per Sample',
+        showlegend: false
+    };
+
+    Plotly.newPlot('bubble', plotData2, layout2);
+    
 }
 
 init();
